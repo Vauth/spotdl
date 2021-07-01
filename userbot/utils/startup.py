b4740357ceffe75bc950c9fc0aba450e4251f5ef
A <75978701+marshallcares@users.noreply.github.com>
2021-07-01 16:33:24 +0430

    Update startup.py

--- userbot/utils/startup.py
+++ userbot/utils/startup.py
@@ -63,9 +63,9 @@ async def startupmessage():
         if BOTLOG:
             Config.CATUBLOGO = await catub.tgbot.send_file(
                 BOTLOG_CHATID,
-                "https://telegra.ph/file/4e3ba8e8f7e535d5a2abe.jpg",
-                caption="**Your CatUserbot has been started successfully.**",
-                buttons=[(Button.url("Support", "https://t.me/catuserbot"),)],
+                "https://telegra.ph/file/8efd97af0d9fb6a21169c.jpg",
+                caption="**Dazai Userbot has been started successfully.**",
+                buttons=[(Button.url("Owner", "https://t.me/feelded"),)],
             )
     except Exception as e:
         LOGS.error(e)
