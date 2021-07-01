64733278f4cd4472cfaad378bd40fbc8c16a83e2
A <75978701+marshallcares@users.noreply.github.com>
2021-07-01 16:38:29 +0430

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -103,17 +103,13 @@ async def bot_start(event):
                 my_mention=my_mention,
             )
         else:
-            start_msg = f"Hey! 👤{mention},\
-                        \nI am {my_mention}'s assistant bot.\
+            start_msg = f"Hey {mention} !\
+                        \nI am Dazai assistant bot.\
                         \nYou can contact to my master from here.\
-                        \n\nPowered by [Catuserbot](https://t.me/catuserbot)"
+                        "
         buttons = [
             (
-                Button.url("Repo", "https://github.com/sandy1709/catuserbot"),
-                Button.url(
-                    "Deploy",
-                    "https://dashboard.heroku.com/new?button-url=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack&template=https%3A%2F%2Fgithub.com%2FMr-confused%2Fcatpack",
-                ),
+                Button.url("Owner", "https://t.me/feelded"),
             )
         ]
     else:
