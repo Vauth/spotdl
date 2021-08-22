46d39edf2497c78133a36ce8b0a8edadabe50639
A <75978701+marshallcares@users.noreply.github.com>
2021-08-22 10:20:28 +0430

    Update tools.py

--- userbot/helpers/tools.py
+++ userbot/helpers/tools.py
@@ -23,12 +23,12 @@ def media_type(message):
 
 async def post_to_telegraph(page_title, html_format_content):
     post_client = TelegraphPoster(use_api=True)
-    auth_name = "CatUserbot"
+    auth_name = "Enjoyer"
     post_client.create_api_token(auth_name)
     post_page = post_client.post(
         title=page_title,
         author=auth_name,
-        author_url="https://t.me/catuserbot17",
+        author_url="https://t.me/DazaiSunBot",
         text=html_format_content,
     )
     return post_page["url"]
