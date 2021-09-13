af482f17070386b5153751be02709cd3c296a5ef
A <75978701+marshallcares@users.noreply.github.com>
2021-09-13 12:53:29 +0430

    Assistant Ping

---
+++ userbot/plugins/botping.py
@@ -0,0 +1,16 @@
+#By @FeelDeD
+from datetime import datetime
+from userbot import catub
+from ..Config import Config
+
+@catub.bot_cmd(
+    pattern="^/ping$",
+    from_users=Config.OWNER_ID,
+)
+async def ping(event):
+    start = datetime.now()
+    catevent = await event.reply("__✮ Pong!__")
+    end = datetime.now()
+    ms = (end - start).microseconds / 1000
+    await catevent.edit(f"🏓 **Pong!**\n`{ms}` ms")
+	
