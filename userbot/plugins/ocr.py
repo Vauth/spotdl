c25bd38f9fe6f8796ef30db927e29cf7e76828fb
marshallcares <75978701+marshallcares@users.noreply.github.com>
2021-10-21 13:41:09 +0330

    Aha

--- userbot/plugins/ocr.py
+++ userbot/plugins/ocr.py
@@ -53,7 +53,7 @@ async def ocr(event):
     "To read text in media."
     reply = await event.get_reply_message()
     if not event.reply_to_msg_id or not reply.media:
-        return await edit_delete(event, "__Reply to a media to read text on it__")
+        return await edit_delete(event, "`Reply to a media to read text on it`")
     catevent = await edit_or_reply(event, "`Reading ...`")
     if not os.path.isdir(Config.TEMP_DIR):
         os.makedirs(Config.TEMP_DIR)
@@ -85,7 +85,7 @@ async def ocr(event):
                 )
             source_lan = LANGUAGES[f"{reply_text.src.lower()}"]
             transl_lan = LANGUAGES[f"{reply_text.dest.lower()}"]
-            tran_text = f"**Translate:**\nFrom {source_lan.title()}({reply_text.src.lower()}) to {transl_lan.title()}({reply_text.dest.lower()}) :\n\n`{reply_text.text}`"
+            tran_text = f"**Translate:**\nFrom {source_lan.title()}({reply_text.src.lower()}) to {transl_lan.title()}({reply_text.dest.lower()}):\n\n`{reply_text.text}`"
             await catevent.edit(
                 f"**Here's what I could read from it:**\n\n`{ParsedText}`\n\n{tran_text}"
             )
