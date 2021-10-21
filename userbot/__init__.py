649972e0dec6f92a3e9043287e2de75491021944
A <75978701+marshallcares@users.noreply.github.com>
2021-10-21 11:54:36 +0330

    Update __init__.py

--- userbot/__init__.py
+++ userbot/__init__.py
@@ -18,7 +18,7 @@ LOGS = logging.getLogger("CatUserbot")
 bot = catub
 
 StartTime = time.time()
-catversion = "3.0.3"
+catversion = "3.0.5"
 
 if Config.UPSTREAM_REPO == "badcat":
     UPSTREAM_REPO_URL = "https://github.com/Jisan09/catuserbot"
