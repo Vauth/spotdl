a5867b443ff5dc60461a177f89308d01dc75b430
Viktor <75978701+marshallcares@users.noreply.github.com>
2022-02-20 11:04:13 +0330

    cc

--- userbot/helpers/utils/tools.py
+++ userbot/helpers/utils/tools.py
@@ -85,3 +85,19 @@ async def take_screen_shot(
     if err:
         LOGS.error(err)
     return thumb_image_path if os.path.exists(thumb_image_path) else None
+
+def get_c_m_message(message_link: str) -> Tuple[Union[str, int], int]:
+    p_m_link = message_link.split("/")
+    chat_id, message_id = None, None
+    if len(p_m_link) == 6:
+        # private link
+        if p_m_link[3] == "c":
+            # for supergroups / channels
+            chat_id, message_id = int("-100" + p_m_link[4]), int(p_m_link[5])
+        elif p_m_link[4] == "UniBorg":
+            # for basic groups / private chats
+            chat_id, message_id = int(p_m_link[4]), int(p_m_link[5])
+    elif len(p_m_link) == 5:
+        # public link
+        chat_id, message_id = str("@" + p_m_link[3]), int(p_m_link[4])
+    return chat_id, message_id
