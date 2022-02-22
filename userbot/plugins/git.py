2b09b7ef0e5325c0beccba67cce84c92193df8e3
Viktor <75978701+marshallcares@users.noreply.github.com>
2022-02-22 16:42:57 +0330

    No.

--- userbot/plugins/git.py
+++ userbot/plugins/git.py
@@ -35,8 +35,7 @@ async def source(e):
     "Source code link of userbot"
     await edit_or_reply(
         e,
-        "Click [here](https://github.com/MarshallCares/SpotifyDL) to open this bot source code\
-        \nClick [here](https://github.com/llawllet/DazaiPack) to open supported link for heroku/railway",
+        "Click [here](https://github.com/llawllet/DeadPack) to open this bot source code\",
     )
 
 
