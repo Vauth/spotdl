ae40660883f34e991b43cead57de0a8205da683b
Viktor <75978701+marshallcares@users.noreply.github.com>
2022-04-23 06:55:09 -0400

    botcmds

--- userbot/plugins/botcmds.py
+++ userbot/plugins/botcmds.py
@@ -1,8 +1,12 @@
+from telethon import events
+from telethon import Button
 from datetime import datetime
 from userbot import catub
 from ..Config import Config
 
 botusername = Config.TG_BOT_USERNAME
+users = list(Config.SUDO_USERS)
+users.append(Config.OWNER_ID)
 
 @catub.bot_cmd(
     pattern=f"^/ping({botusername})?([\s]+)?$",
@@ -14,4 +18,12 @@ async def ping(event):
     end = datetime.now()
     ms = (end - start).microseconds / 1000
     await catevent.edit(f"𝗣𝗼𝗻𝗴\n`{ms}` 𝗆𝗌")
-	
+
+
+@catub.bot_cmd(
+    pattern=f"^/menu({botusername})?([\s]+)?$",
+    from_users=users,
+)
+async def helpmenu(event):
+    b=Button.inline('Click', data='mainmenu')
+    m = await tgbot.send_message(event.chat_id, "**Open Help Menu**", buttons=b, reply_to=event.id)
