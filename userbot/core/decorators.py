f77b0005ebc35b31b9083a2ccd52ef63d01b24f7
Viktor <75978701+marshallcares@users.noreply.github.com>
2022-04-23 06:14:20 -0400

    Update decorators.py

--- userbot/core/decorators.py
+++ userbot/core/decorators.py
@@ -9,19 +9,16 @@ from ..sql_helper.globals import gvarstatus
 
 def check_owner(func):
     async def wrapper(c_q: CallbackQuery):
-        try:
-            auser = list(Config.SUDO_USERS)
-            auser.append(Config.OWNER_ID)
-            if (c_q.query.user_id and c_q.query.user_id in auser):
-                try:
-                    await func(c_q)
-                except FloodWaitError as e:
-                    await asyncio.sleep(e.seconds + 5)
-                except MessageNotModifiedError:
-                    pass
-            else:
-                HELP_TEXT = gvarstatus("HELP_TEXT") or "What could have been."
-                await c_q.answer(HELP_TEXT, alert=True)
-        except Exception:
-                await c_q.answer("You can't use it here.", alert=True)
+        auser = list(Config.SUDO_USERS)
+        auser.append(Config.OWNER_ID)
+        if (c_q.query.user_id and c_q.query.user_id in auser):
+            try:
+                await func(c_q)
+            except FloodWaitError as e:
+                await asyncio.sleep(e.seconds + 5)
+            except MessageNotModifiedError:
+                pass
+        else:
+            HELP_TEXT = gvarstatus("HELP_TEXT") or "What could have been."
+            await c_q.answer(HELP_TEXT, alert=True)
     return wrapper
